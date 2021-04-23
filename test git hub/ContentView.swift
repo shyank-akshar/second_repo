7848a1da10eaefe661a8bed78fd41d34145c3f2a
Shyank Akshar <shyankakshar@Shyanks-MacBook-Pro.local>
2021-04-23 15:59:11 +0530

    hell init

---
+++ test git hub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test git hub
+//
+//  Created by Shyank Akshar on 23/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Nameste, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test git hub/test_git_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_git_hubApp.swift
+//  test git hub
+//
+//  Created by Shyank Akshar on 23/04/21.
+//
+
+import SwiftUI
+
+@main
+struct test_git_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
